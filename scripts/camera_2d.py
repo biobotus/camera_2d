43b36070841b6e4a3b6f0e82dd0cc56bed24c93d
marcaderagon <marc-antoine.deragon@usherbrooke.ca>
2016-11-08 13:57:50 -0500

    camera_2d ROS_MSG -> JPEG

--- scripts/camera_2d.py
+++ scripts/camera_2d.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 
 import cv2
-import img_analysis
+import datetime
 import os
 import rospy
-from std_msgs.msg import String
+import sys
+
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
 
 class Camera2D():
     """This class is responsible for interacting with an HD Camera."""
@@ -16,65 +19,30 @@ class Camera2D():
         self.rate = rospy.Rate(10)  # 10 Hz
 
         # ROS subscriptions
-        self.subscriber = rospy.Subscriber('Capture_2D', String, \
+        self.subscriber = rospy.Subscriber('image_raw', Image, \
                                            self.callback_2d_capture)
 
-        # ROS publishments
-        self.done_capture = rospy.Publisher('Done_Capture', String, queue_size=10)
-        self.error = rospy.Publisher('Error', String, queue_size=10)
-
-        # Camera init
-        self.camera_name = 'usb-HD_Camera_Manufacturer_HD_USB_Camera-video-index0'
-        self.camera_id = self.get_video_id()
-        self.camera = cv2.VideoCapture(self.camera_id)
-        #self.camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 1944)
-        #self.camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 2592)
-        self.camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 1536)
-        self.camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 2048)
-
-        # Take dummy picture
-        self.get_image()
-        print('Camera ready for operation.')
-
-    def get_video_id(self):
-        """Returns the id (#) of /dev/video# of self.camera_name."""
-        camera_connected = True
-        v4l_path = '/dev/v4l/by-id/'
-        try:
-            available_cameras = os.listdir(v4l_path)
-            if self.camera_name in available_cameras:
-                return int(os.path.realpath(os.path.join(v4l_path, \
-                                                self.camera_name))[-1])
-        except OSError:
-            pass
-        raise EnvironmentError('Requested camera is not connected.')
-
-    def get_image(self):
-        for i in xrange(15):
-            self.camera.grab()
-            _, img = self.camera.retrieve()
-        return img
+        self.bridge = CvBridge()
+        self.flag = False
 
     def callback_2d_capture(self, data):
-        filename = data.data
-        #cwd = os.getcwd()
-        cwd = '/home/ubuntu/Desktop/Petri/'
+        self.flag = True
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+        except CvBridgeError as e:
+            print(e)
+            return
+
+        filename = "{0}.jpg".format(str(datetime.datetime.now())[:19])
+        cwd = '/home/marc/Desktop/photos'
         file_path = os.path.join(cwd, filename)
-        # if not self.camera.grab():
-        #     print('Error grabbing camera frame.')
-        #     return
-        # _, img = self.camera.retrieve()
 
-        img = self.get_image()
-
-        # img_dict = {filename: img}
-        # img_analysis.imshow(img_dict)
-        cv2.imwrite(file_path, img)
+        cv2.imwrite(file_path, cv_image)
         print("Image {0} saved in {1}".format(filename, cwd))
-        self.done_capture.publish(file_path)
 
     def listener(self):
-        rospy.spin()
+        while not self.flag:
+            rospy.sleep(0.1)
 
 if __name__ == '__main__':
     try:
