7278fe714a32c074951ffdd277f76cd7637dc414
biobotus <biobotus@gmail.com>
2016-12-04 13:44:47 -0500

    Delete ROS build files and update gitignore

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/biobot_ros_jtk/src/camera_2d
-# Build directory: /home/ubuntu/biobot_ros_jtk/src/camera_2d
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
